482fb71ce6979f9c50e3fcf91512e5940f4fc35f
Pedram Shirforoushan <Pedram.Shirforoushan@yh.nackademin.se>
2020-04-26 03:16:44 +0200

    Added 16 records.

--- ejb_mod/src/main/java/ejb/CreateDataBean.java
+++ ejb_mod/src/main/java/ejb/CreateDataBean.java
@@ -30,10 +30,43 @@ public class CreateDataBean implements CreateDataBeanLocal {
             final Record r2 = new Record("Excalibur", "Live In Brocéliande", "excalibur-1.jpg", 209);
             final Record r3 = new Record("John Hiatt", "Ottawa", "johnhiatt-1.jpg", 199);
             final Record r4 = new Record("Streaplers", "En gång till", "streaplers-1.jpg", 79);
+            final Record r5 = new Record("Adele", "25", "adele-25.jpg", 99);
+            final Record r6 = new Record("Avicii", "Stories", "avicii-stories.jpg", 225);
+            final Record r7 = new Record("David Bowe", "The man who sold the world", "bowe-soldworld.jpg", 175);
+            final Record r8 = new Record("The Clash", "London calling", "clash-london.jpg", 50);
+            final Record r9 = new Record("Elton John", "Diamonds", "ej-diamonds.jpg", 125);
+            final Record r10 = new Record("Elegia", "New order", "elegia-order.jpg", 99);
+            final Record r11 = new Record("Eminem", "Encore", "eminem-encore.jpg", 199);
+            final Record r12 = new Record("Five Finger Death Punch", "F8", "ffdp-f8.jpg", 50);
+            final Record r13 = new Record("Imagine Dragons", "Evolve", "id-evolve.jpg", 99);
+            final Record r14 = new Record("Bon Jovi", "2020", "jovi-2020.jpg", 149);
+            final Record r15 = new Record("Michael Jackson", "Thriller", "mj-thriller.jpg", 199);
+            final Record r16 = new Record("Michael Jackson", "Bad", "mj-bad.jpg", 199);
+            final Record r17 = new Record("N.W.A", "Straight outta compton", "nwa-compton.jpg", 99);
+            final Record r18 = new Record("Queen", "Greatest hits", "queen-greatest.jpg", 225);
+            final Record r19 = new Record("Frank Sinatra", "Ultimate Sinatra", "sinatra-ultimate.jpg", 500_000);
+            final Record r20 = new Record("Slipknot", "Iowa", "slipknot-iowa.jpg", 275);
+
             recordList.add(r1);
             recordList.add(r2);
             recordList.add(r3);
             recordList.add(r4);
+            recordList.add(r5);
+            recordList.add(r6);
+            recordList.add(r7);
+            recordList.add(r8);
+            recordList.add(r9);
+            recordList.add(r10);
+            recordList.add(r11);
+            recordList.add(r12);
+            recordList.add(r13);
+            recordList.add(r14);
+            recordList.add(r15);
+            recordList.add(r16);
+            recordList.add(r17);
+            recordList.add(r18);
+            recordList.add(r19);
+            recordList.add(r20);
 
             recordList.forEach(record -> em.persist(record));
 
